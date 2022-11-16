9bf5eae81180fcdba155a23294a6f970bb3ac025
mohammed yassine dabboussi <mohammed-yassine.dabboussi@laplateforme.io>
2022-11-16 16:35:09 +0100

    job 01 done

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -1,7 +1,7 @@
 <?php 
-    //Parcourir les nombres compris entre 0 et 1337
-    for ($i = 1; $i <= 1336; $i++) {
-        
+    //Mohammed Yassine Dabboussi Runtrack 2 Jour 2 Job 01
+
+    for ($i = 1; $i <= 1336; $i++) {//Parcourir les nombres compris entre 0 et 1337        
         //condition sur le nombre 42
         if ($i === 42) {
             echo "<b><u> $i </u></b>";//affichage en gras et souligné du nombre
