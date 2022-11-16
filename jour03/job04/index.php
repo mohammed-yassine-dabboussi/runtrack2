35fe2dc418b1615e02930ddd6de5a0b77afdc85e
mohammed yassine dabboussi <mohammed-yassine.dabboussi@laplateforme.io>
2022-11-16 16:01:54 +0100

    job 04 done

--- jour03/job04/index.php
+++ jour03/job04/index.php
@@ -1,5 +1,5 @@
 <?php
-    //Mohammed Yassine Dabboussi Runtrack2 Jour 3 Job 4// 
+    //Mohammed Yassine Dabboussi Runtrack 2 Jour 3 Job 4// 
 
     $str= "Dans l'espace, personne ne vous entend crier";
     $c= 0; // initialiser le nombre de caractère à 0 
