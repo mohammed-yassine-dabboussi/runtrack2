e7ff53c2c67ab4b1da95844926350571e9bc9008
mohammed yassine dabboussi <mohammed-yassine.dabboussi@laplateforme.io>
2022-11-16 16:05:28 +0100

    job 05 done

--- jour03/job05/index.php
+++ jour03/job05/index.php
@@ -1,23 +1,24 @@
 <?php
-$str= "On n'est pas le meilleur quand on le croit mais quand on le sait.";
-$voyelles= ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U', 'y', 'Y'];
-$dic = [
-    "consonnes" => 0,
-    "voyelles" => 0,
-];
-$i=0;
-while ($str[$i] != '.'){
-        for($j = 0; $j < 12; $j++){
-            if ($str[$i] === $voyelles[$j] ){ 
-                $dic["voyelles"]++;
-                break;
-            } else if ($j==11 AND $str[$i]!=" " AND $str[$i]!="." AND $str[$i]!="'" AND $str[$i]!=","){
-                $dic["consonnes"]++;
-            }
-        }
-    $i++;
-}
+    //Mohammed Yassine Dabboussi Runtrack 2 Jour 3 Job 5// 
 
+    $str= "On n'est pas le meilleur quand on le croit mais quand on le sait.";
+    $voyelles= ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U', 'y', 'Y']; // tableau contient toutes les voyelles en majuscules et en miniscules 
+    $dic = [
+        "consonnes" => 0,
+        "voyelles" => 0,
+    ];
+    $i=0; // initialiser le compteur
+    while ($str[$i] != '.'){ // arrêter la boucle quand il y'a un point à la fin de la phrase
+            for($j = 0; isset($voyelles[$j]); $j++){ // parcourir le tableau des voyelles pour comparer avec la chaine de caractère str
+                if ($str[$i] === $voyelles[$j] ){  
+                    $dic["voyelles"]++; //incrémenter le nombre de voyelles
+                    break;
+                } else if ($j==11 AND $str[$i]!=" " AND $str[$i]!="." AND $str[$i]!="'" AND $str[$i]!=","){
+                    $dic["consonnes"]++;//incrémenter le nombre des consonnes
+                }
+            }
+        $i++;
+    }
 
 ?>
 
