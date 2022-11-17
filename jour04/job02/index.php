74c01632e8084835b0a446e505e29b9c8d0a64c8
mohammed yassine dabboussi <mohammed-yassine.dabboussi@laplateforme.io>
2022-11-17 13:28:22 +0100

    job 02 done

---
+++ jour04/job02/index.php
@@ -0,0 +1,67 @@
+<html>
+    <!-- Mohammed Yassine Dabboussi Runtrack 2 Jour 4 Job 2-->
+
+    <head>
+        <meta charset="UTF-8">
+        <title>jour 4 job 2</title>
+    </head>
+
+    <body>
+        
+        <form action="" method="get">
+        <table >
+            <tr>
+                <td>Votre nom :   </td>
+                <td><input type="text" name="nom" /></td>
+            </tr>
+            <tr>
+                <td>   </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Votre prenom :</td>
+                <td><input type="text" name="prenom" /></td>
+            </tr>
+            <tr>
+                <td>Votre âge :</td>
+                <td><input type="text" name="age" /></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="OK"></td>
+                
+            </tr>
+        </table>
+        </form>
+
+        <br>
+        
+        <table border : 1 px >
+            <tr>
+                <th>Argument</th>
+                <th>Valeur</th>
+            </tr>
+           
+            <tr>
+                <td>
+                <?php 
+                        foreach($_GET as $args => $value){
+                            $long=0;
+                            for($i = 0; isset($value[$i]); $i++){
+                                $long++;
+                            }
+                            if ($long>0){ 
+                            echo "<tr>";
+                            echo "<td>".$args."</td>";
+                            echo "<td>".$value."</td>";
+                            echo "</tr>";
+                            }
+            
+                        }
+                    ?>  
+                </td>
+            </tr>
+        </table>
+
+    </body>
+</html>
