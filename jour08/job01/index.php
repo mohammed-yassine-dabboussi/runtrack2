0e90615a367d5f07f031b3fa25ea907f90024c4c
mohammed yassine dabboussi <mohammed-yassine.dabboussi@laplateforme.io>
2022-11-23 14:47:00 +0100

    job en cours

---
+++ jour08/job01/index.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+//si la variable existe, incrémenter de 1 sinon la créer et lui donner la valeur 1
+isset($_SESSION['nbvisites']) ? $_SESSION['nbvisites']++ : $_SESSION['nbvisites'] = 1;
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jour 8 job 01</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="reset">Remettre le compteur à zéro :</label>
+        <input type="submit" value="reset" name="reset">
+    </form>
+    <br>
+    <br>
+    <?php
+
+
+    if (isset($_POST['reset'])) {
+        $_SESSION['nbvisites'] = 1;
+    }
+    echo "Vous avez visité cette page ".$_SESSION['nbvisites']." fois";
+    ?>
---
+++ jour08/job02/index.php
@@ -0,0 +1,45 @@
+<?php
+    if (isset($_COOKIE['nbvisites'])) {
+        setcookie('nbvisites', $_COOKIE['nbvisites'] + 1, time() + 180*24*3600);
+    }
+    else {
+        setcookie('nbvisites', '1', time() + 365*24*3600);
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jour 8 job 2</title>
+</head>
+<body>
+
+
+    <form action="" method="post">
+        <br>
+        <br>
+        <label for="reset">Remettre le compteur à zéro :</label><br>
+        <input type='submit' value='reset' name='reset'>
+        <br>
+        <br>
+    </form>
+
+    <?php
+
+    if (isset($_POST['reset'])) {
+        setcookie('nbvisites', '1', time() -3600);
+        setcookie('nbvisites', '1', time() + 365*24*3600);
+        $_COOKIE['nbvisites'] = 1;
+    }
+
+    ?>
+
+    <?php
+    echo "Vous avez visité cette page " . $_COOKIE['nbvisites'] . " fois";
+    ?>
+</body>
+</html>
