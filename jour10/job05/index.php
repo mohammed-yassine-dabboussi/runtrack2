ec02dfabe291b30139dd4d3e9644138e37f45198
mohammed yassine dabboussi <mohammed-yassine.dabboussi@laplateforme.io>
2022-11-25 14:43:08 +0100

    job 4 , 5 et 6 done

---
+++ jour10/job05/index.php
@@ -0,0 +1,61 @@
+<?php
+
+$mysqli = new mysqli('localhost', 'root', '', 'jour09');
+
+$request = $mysqli -> query("SELECT * FROM `etudiants` WHERE `naissance` < '2004-01-01'");
+
+// echo "afichage fetch all <br>";
+// $result_fetch_all = $request -> fetch_all();
+// var_dump($result_fetch_all);
+
+//  echo "affichage fetch array <br>";
+//  $result_fetch_array = $request -> fetch_array();
+//  var_dump($result_fetch_array);
+
+//   echo"affichage fetch assoc <br>";
+//   $result_fetch_assoc = $request -> fetch_assoc();
+//   var_dump($result_fetch_assoc);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jour 10 job 05</title>
+        
+</head>
+
+<body>
+    <h1> Jour 10 Job 05 </h1>
+    <table border : solid>
+            <thead>
+                <tr>
+                    <th>Prénom</th>
+                    <th>Nom</th>
+                    <th>Naissance</th>
+                    <th>Sexe</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    while(($result = $request -> fetch_array()) != null)
+                    {
+                        echo "<tr>";
+                        echo "<td>".$result['prenom']."</td>";
+                        echo "<td>".$result['nom']."</td>";
+                        echo "<td>".$result['naissance']."</td>";
+                        echo "<td>".$result['sexe']."</td>";
+                        echo "<td>".$result['email']."</td>";
+                        echo "</tr>";
+                    }
+                ?>
+    </table>
+<?php $mysqli->close(); ?>
+</body>
+
+</html>
