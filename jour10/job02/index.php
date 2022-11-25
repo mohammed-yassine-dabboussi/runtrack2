b6162d58637e89f92484e839b14aee9cbc95e447
mohammed yassine dabboussi <mohammed-yassine.dabboussi@laplateforme.io>
2022-11-25 13:14:16 +0100

    job 02 done

--- jour10/job02/index.php
+++ jour10/job02/index.php
@@ -25,12 +25,12 @@ $request = $mysqli -> query("select * from salles");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>job01 jour 10</title>
+    <title>jour 10 job02</title>
         
 </head>
 
 <body>
-
+    <h1>Jour 10 Job 02</h1>
     <table border : solid>
             <thead>
                 <tr>
